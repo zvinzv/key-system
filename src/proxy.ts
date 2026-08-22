524db2086993bedc5af9492bb1d21ae964496c68
Murtadha Dhafer <eng.murtadha.dhafer@gmail.com>
2026-08-23 00:07:20 +0300

    Edit proxy 5 time

--- src/proxy.ts
+++ src/proxy.ts
@@ -9,7 +9,7 @@ export async function proxy(request: NextRequest) {
   // This is the recommended approach to optimistically redirect users
   // We recommend handling auth checks in each page/route
   if (!sessionCookie) {
-    return NextResponse.redirect(new URL("/", request.url));
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
   console.log("[proxy] getSession:", Date.now() - start, "ms");
