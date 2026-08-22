e77b0a00d4538aa0c883aa289b0f79b927a4b5e3
Murtadha Dhafer <eng.murtadha.dhafer@gmail.com>
2026-08-22 23:53:10 +0300

    Edit proxy for 3 time

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,16 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
-import { auth } from "@/lib/auth";
+import { getSessionCookie } from "better-auth/cookies";
 
 export async function proxy(request: NextRequest) {
   const start = Date.now();
 
-  const session = await auth.api.getSession({
-    headers: request.headers,
-  });
+  const sessionCookie = getSessionCookie(request);
+  // THIS IS NOT SECURE!
+  // This is the recommended approach to optimistically redirect users
+  // We recommend handling auth checks in each page/route
+  if (!sessionCookie) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
 
   console.log("[proxy] getSession:", Date.now() - start, "ms");
 
-  if (!session) {
+  if (!sessionCookie) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
