bd23a56ad70fc97966fd7f6527826d6390136ae6
Murtadha Dhafer <eng.murtadha.dhafer@gmail.com>
2026-08-22 23:45:51 +0300

    Edit proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,10 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
-import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
 
 export async function proxy(request: NextRequest) {
   const session = await auth.api.getSession({
-    headers: await headers(),
+    headers: request.headers,
   });
 
   if (!session) {
